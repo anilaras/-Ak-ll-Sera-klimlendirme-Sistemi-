1182ecd32a840d3bdd8b7ae4bfee2c4b10bad6c6
kayailkerugur <kayailkerugur@gmail.com>
2024-11-24 00:12:51 +0300

    some issue was fixed, try 2

---
+++ SGACSystemApp/Core/View/TabView.swift
@@ -0,0 +1,18 @@
+//
+//  TabView.swift
+//  SGACSystemApp
+//
+//  Created by İlker Kaya on 24.11.2024.
+//
+
+import SwiftUI
+
+struct TabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabView()
+}
---
+++ SGACSystemApp/SGACSystemAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  SGACSystemAppApp.swift
+//  SGACSystemApp
+//
+//  Created by İlker Kaya on 18.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct SGACSystemAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
